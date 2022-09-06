7ab9deb6bb32b2e3b35b63c4f16af4e46363aa61
the96 <aar30px2@gmail.com>
2022-09-06 23:54:16 +0900

    server statusの取得コマンド、serverのWakeOnLanコマンドの実装

--- go.mod
+++ go.mod
@@ -4,11 +4,15 @@ go 1.19
 
 require (
 	github.com/bwmarrin/discordgo v0.26.1
+	github.com/go-ping/ping v1.1.0
 	github.com/joho/godotenv v1.4.0
 )
 
 require (
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 )
--- main.go
+++ main.go
@@ -3,32 +3,114 @@ package main
 import (
 	"fmt"
 	"os"
+	"net"
 	"github.com/joho/godotenv"
 	"github.com/bwmarrin/discordgo"
+	"github.com/go-ping/ping"
 )
 
 func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	clientId := os.Getenv("CLIENT_ID")
 	u := m.Author
 	fmt.Printf("%20s %20s(%20s) > %s\n", m.ChannelID, u.Username, u.ID, m.Content)
-	if u.ID != clientId {
-		fmt.Println(m.Type)
-		sendMessage(s, m.ChannelID, u.Mention()+"なんか喋った!")
-		sendReply(s, m.ChannelID, "test", m.Reference())
-	}
 }
 
-func onInteractionCreate(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	fmt.Printf("interaction: %#v\n", interaction.Interaction)
-	interactionType := interaction.Type
-	interactionData := interaction.Data
+func pingServer() bool {
+	address := os.Getenv("SERVER_IP_ADDRESS")
+	pinger, err := ping.NewPinger(address)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	count := 1
+	pinger.Count = count
+	pinger.Timeout = 1_000_000_000 // nano seconds
+	pinger.Run() // blocks until finished
+	stats := pinger.Statistics() // get send/receive/rtt stats
+	fmt.Println(stats)
 
-	fmt.Printf("type:%s data:%s\n", interactionType, interactionData)
-	sendMessage(session, interaction.ChannelID, "receive command")
+	return (stats.PacketsRecv == count)
+}
+
+func wakeOnLan() bool {
+	// https://monakaice88.hatenablog.com/entry/2019/10/29/070000
+	targetMacAddress := os.Getenv("SERVER_MAC_ADDRESS")
+	// localIpAddress   := net.ResolveUDPAddr("udp", ":0")
+	// conn, err := net.Dial("udp", os.Getenv("SERVER_IP_ADDRESS")+":9")
+
+	targetIpAddress, _ := net.ResolveUDPAddr("udp", os.Getenv("SERVER_IP_ADDRESS")+":9")
+    conn, err := net.DialUDP("udp", nil, targetIpAddress)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	packetPrefix := make([]byte, 6)
+	for i := range packetPrefix {
+		packetPrefix[i] = 0xFF
+	}
+
+	hw, err := net.ParseMAC(targetMacAddress)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+
+	sendPacket := make([]byte, 0)
+	sendPacket = append(sendPacket, packetPrefix...)
+	for i := 0; i < 16; i++ {
+		sendPacket = append(sendPacket, hw...)
+	}
+
+	_, err = conn.Write(sendPacket)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	return true
+}
+
+
+func onInteractionCreate(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+	// fmt.Printf("interaction: %#v\n", interaction.Interaction)
+	// interactionType := interaction.Interaction.Type
+	// interactionData := interaction.Interaction.Data
+	// fmt.Printf("type:%s data:%s\n", interactionType, interactionData)
+	
+	interactionType := interaction.Interaction.Type
+	fmt.Printf("type: %s\n", interactionType)
+
+	data := interaction.Interaction.Data.(discordgo.ApplicationCommandInteractionData)
+	options := data.Options
+	if (len(options) != 1) {
+		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "sorry, the command you sent is unknown command...",
+			},
+		})
+	}
+	
+	name := options[0].Name
+	response := ""
+	switch name {
+	case "status":
+		status := "server is sleeped. :zzz:"
+		if (pingServer()) {
+			status = "server is running! :sunny:"
+		}
+		response = fmt.Sprintf("server status: %s\n", status)
+	case "wake":
+		wakeOnLan()
+		response = "awaking server! please wait a moment."
+	}
+
 	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: "command received!",
+			Content: response,
 		},
 	})
 }
